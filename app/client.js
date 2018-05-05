adeb97068209d6f20f202dcee4c155a59c53f818
srini-msi <srinivasrk92@gmail.com>
2018-05-05 12:47:18 -0400

    added bootstrap and jquery to sapper

--- app/client.js
+++ app/client.js
@@ -1,6 +1,18 @@
 import { init } from 'sapper/runtime.js';
 import { routes } from './manifest/client.js';
 
+import jquery from 'jquery';
+
+// export for others scripts to use
+
+window.$ = jquery;
+
+window.jQuery = jquery;
+
+import 'bootstrap';
+
+import "bootstrap/scss/bootstrap.scss";
+
 // `routes` is an array of route objects injected by Sapper
 init(document.querySelector('#sapper'), routes);
 
